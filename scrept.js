b8203317943d93a4ff1fc19f95544efe81fd0d6b
Eshpolatov-Sardor <167778318+Eshpolatov-Sardor@users.noreply.github.com>
2024-05-05 13:56:41 +0500

    sardor

--- scrept.js
+++ scrept.js
@@ -1,25 +1,42 @@
-let kam = document.querySelector(".kam")
-let img = document.querySelector(".img")
-let button = document.querySelector(".button")
-let body =document.body;
-button.addEventListener("click",()=>{
-    body.classList.toggle('dark');
-})
-button.style.color="red"
-button.style.width="300px"
-button.style.height="50px"
-fetch("https://jsonplaceholder.typicode.com/users").then((item)=>{
-    return item.json();
-}).then((data)=>{
-    console.log(data);
-    return(
-        data.map(item => {
-            kam.innerHTML +=`
-            <h1>${item.name}</h1>
-            <h1>${item.username}</h1>
-            <h1>${item.email}</h1>
-            <h1>${item.address.street}</h1>
-            `
-        })
-    )
-})
+let kam = document.querySelector(".kam");
+let img = document.querySelector(".img");
+let button = document.querySelector(".button");
+let body = document.body;
+
+button.addEventListener("click", () => {
+  body.classList.toggle('dark');
+});
+
+button.style.color = "red";
+button.style.width = "300px";
+button.style.height = "50px";
+img.style.height="10px";
+
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    data.products.forEach((item) => {
+      const h1 = document.createElement('h1');
+      h1.textContent = item.title;
+      kam.appendChild(h1);
+
+      const h2 = document.createElement('h2');
+      h2.textContent = item.description;
+      kam.appendChild(h2);
+
+      const discount = document.createElement('span');
+      discount.textContent = `Discount: ${item.discountPercentage}`;
+      kam.appendChild(discount);
+
+      const h3 = document.createElement('h3');
+      h3.textContent = item.description;
+      kam.appendChild(h3);
+
+      item.images.forEach((imageUrl) => {
+        const image = document.createElement('img');
+        image.src = imageUrl;
+        img.appendChild(image);
+      });
+    });
+  });
+  
